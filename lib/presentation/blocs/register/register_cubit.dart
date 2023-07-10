bdf19b03b0570145fe54782d2b720b07461f64b9
ftorreslop <ftorreslop@gmail.com>
2023-07-10 20:10:57 +0000

    another change

--- lib/presentation/blocs/register/register_cubit.dart
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -7,4 +7,26 @@ part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterFormState> {
   RegisterCubit() : super(const RegisterFormState());
+
+  void onSubmit() {
+    print('Submit: $state');
+  }
+
+  void usernameChange(String value) {
+    emit(state.copyWith(
+      username: value,
+    ));
+  }
+
+  void emailChange(String value) {
+    emit(state.copyWith(
+      email: value,
+    ));
+  }
+
+  void passwordChange(String value) {
+    emit(state.copyWith(
+      password: value,
+    ));
+  }
 }
